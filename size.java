911e376c64f4254f306610c118623b0eaf64b4c6
Nadish KB <nadishkb@gmail.com>
2025-12-01 21:03:09 +0530

    Bit size of primitive datatype

---
+++ size.java
@@ -0,0 +1,21 @@
+class Size_{
+    public static void main(String[] args) {
+        //primitive data type
+        byte by = 100;
+        System.out.println(Byte.SIZE);
+        short sh = 100;
+         System.out.println(Short.SIZE);
+        int in = 100;
+         System.out.println(Integer.SIZE);
+        long lo = 100;
+         System.out.println(Long.SIZE);
+
+        float fl = 1.0f;
+         System.out.println(Float.SIZE);
+        double dl = 1.0;
+         System.out.println(Double.SIZE);
+
+        boolean bl = true;
+    }
+    
+}
